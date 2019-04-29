39326a7ea15296d1cec3b0ba81c9da3a5712e806
Callum Stringer <callumstringer10@gmail.com>
2019-04-29 15:50:56 +0200

    added mousemotiontracking / GUI Changes

---
+++ Buuretraum/BuuretraumMaven/src/main/java/com/buuretraum/main/MouseMotion.java
@@ -0,0 +1,23 @@
+package com.buuretraum.main;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class MouseMotion implements MouseMotionListener{
+
+	public MouseMotion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("DRAGGED");
+	}
+
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("MOVED");
+		
+	}
+
+}
